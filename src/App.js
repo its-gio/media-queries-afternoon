0fb156d64b6ced02615415da1fdff197692d663b
Giovani Aguirre <me@its-g.io>
2020-02-17 14:59:39 -0600

    Setup for navbar complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Hero/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Hero/Header.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  state = {
+    isOpen: false
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <h1>Start Bootstrap</h1>
+
+          <ul className="desk-nav">
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Contact</li>
+          </ul>
+          
+          <div className="mobile-nav--btn" onClick={() => this.setState({ isOpen: !this.state.isOpen })}>
+            <p>Menu</p>
+            <img className="mobile-nav--btn__ham" src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.ya-webdesign.com%2Fimages%2Fhamburger-menu-icon-png-white-6.png" alt=""/>
+          </div>
+        </nav>
+        
+
+      </div>
+    )
+  }
+}
