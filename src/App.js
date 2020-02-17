69753b1649191f8cde9c47d92f802141b1a35fc0
Giovani Aguirre <me@its-g.io>
2020-02-17 17:05:06 -0600

    Added styles for section titles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Hero/Header';
 import Hero from './components/Hero/Hero';
+import Service from './components/Service';
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Service />
     </div>
   );
 }
---
+++ src/components/Service.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Service extends Component {
+  render() {
+    return (
+      <section>
+        <div className="section-title">
+          <h3>Services</h3>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+    )
+  }
+}
