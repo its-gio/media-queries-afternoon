5320000690311091df0d16f026d9148038e636a5
Giovani Aguirre <me@its-g.io>
2020-02-17 16:00:32 -0600

    Hero section views complete (Mobile and desktop)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Hero/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+  render() {
+    return (
+      <section className="hero">
+        <div className="hero--content">
+          <h4>Welcome To Our Studio!</h4>
+          <h2>It's Nice To Meet You</h2>
+          <button>Tell Me More</button>
+        </div>
+      </section>
+    )
+  }
+}
