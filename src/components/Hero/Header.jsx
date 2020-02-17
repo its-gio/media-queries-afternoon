dfc0b8f5fa3e23d98c3e2e35ad2b5ae38538b50f
Giovani Aguirre <me@its-g.io>
2020-02-17 15:19:28 -0600

    Navbar mobile button working.

--- src/components/Hero/Header.jsx
+++ src/components/Hero/Header.jsx
@@ -19,13 +19,21 @@ export default class Header extends Component {
             <li>Contact</li>
           </ul>
           
-          <div className="mobile-nav--btn" onClick={() => this.setState({ isOpen: !this.state.isOpen })}>
+          <div className="mobile-nav-btn" onClick={() => this.setState({ isOpen: !this.state.isOpen })}>
             <p>Menu</p>
-            <img className="mobile-nav--btn__ham" src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.ya-webdesign.com%2Fimages%2Fhamburger-menu-icon-png-white-6.png" alt=""/>
+            <img className="mobile-nav-btn__ham" src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.ya-webdesign.com%2Fimages%2Fhamburger-menu-icon-png-white-6.png" alt=""/>
           </div>
         </nav>
-        
 
+        <menu className={this.state.isOpen ? "mobile-nav mobile-nav--open" : "mobile-nav" }>
+          <ul className="mobile-nav--menu">
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Contact</li>
+          </ul>
+        </menu>
       </div>
     )
   }
