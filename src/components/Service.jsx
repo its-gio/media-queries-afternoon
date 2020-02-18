11aa14e39a1dd87125d2ba975e69aa3959777b64
Giovani Aguirre <me@its-g.io>
2020-02-18 14:54:12 -0600

    Completed Service section views (mobile and desktop)

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,18 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import * as FontAwesome from 'react-icons/fa'
 
 export default class Service extends Component {
   render() {
     return (
-      <section>
+      <section className="section">
         <div className="section-title">
           <h3>Services</h3>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
         </div>
 
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="services-grid">
+          <div className="services-grid--item">
+            <div className="services-grid--item__icon-container">
+              <FontAwesome.FaShoppingCart className="ShoppingCart" />
+            </div>
+            <h4>E-Commerce</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
+          
+          <div className="services-grid--item">
+            <div className="services-grid--item__icon-container">
+              <FontAwesome.FaLaptop className="Laptop" />
+            </div>
+            <h4>Responsive Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
+
+          <div className="services-grid--item">
+            <div className="services-grid--item__icon-container">
+              <FontAwesome.FaLock className="Lock" />
+            </div>
+            <h4>Web Security</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
         </div>
       </section>
     )
